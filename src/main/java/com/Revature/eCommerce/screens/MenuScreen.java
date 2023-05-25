6a04d246b864b92d0114a6ec1c8df79ca2244638
jeikuru <officialjbyrd@gmail.com>
2023-05-25 16:33:12 -0500

    Updating Menu and Adding Browse Screen File

--- src/main/java/com/Revature/eCommerce/screens/MenuScreen.java
+++ src/main/java/com/Revature/eCommerce/screens/MenuScreen.java
@@ -14,9 +14,48 @@ public class MenuScreen implements IScreen {
 
     @Override
     public void start(Scanner scan) {
-        System.out.println("Welcome to the menu screen " + session.getUsername() + "!");
-        scan.nextLine();
+        String input = "";
+
+        exit: {
+            while (true) {
+                clearScreen();
+                System.out.println("Welcome to the menu screen " + session.getUsername() + "!");
+                System.out.println("\nPlease select your option:\n");
+                System.out.println("Press [1] to Browse Products.");
+                System.out.println("Press [2] to Search Products.");
+                System.out.println("Press [3] to View Cart.");
+                System.out.println("Press [4] to Checkout");
+                System.out.println("Press [5] to View History");
+                System.out.println("Press [X] to exit the application.");
+
+                System.out.print("\nEnter: ");
+                input = scan.nextLine();
+
+                switch (input.toLowerCase()) {
+                    case "1":
+                        break exit;
+                    case "2":
+                        break exit;
+                    case "3":
+                        break exit;
+                    case "4":
+                        break exit;
+                    case "5":
+                        break exit;
+                    case "x":
+                        System.out.println("\nGoodbye!");
+                        break exit;
+                    default:
+                        clearScreen();
+                        System.out.println("Invalid option!");
+                        System.out.print("\nPress enter to continue...");
+                        scan.nextLine();
+                        break;
+                }
+            }
+        }
     }
+    
 
     private void clearScreen() {
         System.out.print("\033[H\033[2J");
