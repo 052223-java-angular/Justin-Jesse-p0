3c214bec244541e76bde1b04da4c9004c9fa2687
jribezzo <116099453+jribezzo@users.noreply.github.com>
2023-05-31 08:37:26 -0400

    Comments

--- src/main/java/com/Revature/eCommerce/services/PaymentService.java
+++ src/main/java/com/Revature/eCommerce/services/PaymentService.java
@@ -1,6 +1,14 @@
 package com.Revature.eCommerce.services;
 
+/**
+ * Validates payment credentials
+ */
 public class PaymentService {
+    /**
+     * Verifies if cc number is 16 digits and numbers
+     * @param cc - number
+     * @return - true/false
+     */
     public boolean isValidCC(String cc)
     {
 
@@ -9,17 +17,23 @@ public class PaymentService {
         }
         return false;
     }
+
+    /**
+     * verifies if exp date is valid
+     * @param exp - exp number
+     * @return true/false
+     */
     public boolean isValidExp(String exp)
     {
-        String [] date = exp.split("/");
-        if(date.length != 2)
+        String [] date = exp.split("/");// splits the 4 num at the slash
+        if(date.length != 2) // checks array length
         {
             return false;
         }
         else
         {
             int month = Integer.parseInt(date[0]);
-            if (month > 0 && month <13)
+            if (month > 0 && month <13)//verify if its 1-12
             {
                 int year = Integer.parseInt(date[1]);
                 return true;
@@ -28,6 +42,11 @@ public class PaymentService {
         return false;
     }
 
+    /**
+     * checks if cvv is valid 3 digits
+     * @param cvv - number entered
+     * @return true/false
+     */
     public boolean isValidCVV(String cvv)
     {
         return cvv.length() == 3 && cvv.matches("^[0-9]{3}$");
